84b98547274368edb3255f3276cea7f6d8be7ef3
Nicolas <nicolasbonemme@hotmail.com>
2017-12-01 11:22:24 +0100

    Debut site ASP

--- WebApplication/Site.Master.cs
+++ WebApplication/Site.Master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FilmDTOLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,26 @@ namespace WebApplication
 {
     public partial class SiteMaster : MasterPage
     {
+        ServiceHostReference.ToolsBDClient s;
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                s = new ServiceHostReference.ToolsBDClient();
+                MovieGridView.DataSource = s.GetFilms("Films", 0);
+                MovieGridView.DataBind();
+                List<FilmDTO> film = new List<FilmDTO>();
+                //film = s.GetFilms("Films", 0);
+                
+            }
+            catch (Exception ex)
+            {
+                //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Impossible d atteindre le service WCF')", true);
+            }
+        }
+
+        protected void MovieGridView_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
